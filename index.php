b28264ff0c57294737eb1dd42a0d3d9832bddf18
thomasbetend <thomas.betend@yahoo.fr>
2022-11-23 12:59:08 +0100

    date flights

--- index.php
+++ index.php
@@ -24,12 +24,12 @@ require('./View/FlightsListView.class.php');
 require('./View/PromosListView.class.php');
 require('./View/ReservationFlightsListView.class.php');
 
-require('./Controller/Main.class.php');
+require('./Controller/HomeController.class.php');
 require('./Controller/ReservationsController.class.php');
 require('./Controller/UserController.class.php');
 require('./Controller/CartController.class.php');
 require('./Controller/FlightsController.class.php');
-
+require('./Controller/CalendarController.class.php');
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
@@ -38,12 +38,7 @@ $split= explode("/", $uri);
 //var_dump($split); die;
 
 if ('/accueil' === $uri || '/index.php' === $uri) {
-    Main::home();
-}
-
-if ('/reservations' === '/' . $split[1]) {
-    Security::isloggedIn($_SESSION);
-    ReservationsController::getList($split[2]);
+    HomeController::home();
 }
 
 if ('/connexion' === $uri) {
@@ -59,9 +54,28 @@ if ('/inscription' === $uri) {
     UserController::getSignup();
 }
 
+if ('/reservations' === '/' . $split[1]) {
+    Security::isloggedIn($_SESSION);
+    ReservationsController::getList($split[2]);
+}
+
 if ('/reservation-validation' === '/' . $split[1]) {
     Security::isloggedIn($_SESSION);
     ReservationsController::getValidation($split[2]);
+}
+
+if('/reservation-impossible' == $uri){
+    ReservationsController::impossible();
+}
+
+if('/reservation-details' === '/' . $split[1]){
+    Security::isloggedIn($_SESSION);
+    ReservationsController::getDetails($split[2]);
+}
+
+if('/reservation-acceptee' == $uri){
+    Security::isloggedIn($_SESSION);
+    ReservationsController::success();
 }
 
 if ('/panier' === $uri) {
@@ -78,28 +92,14 @@ if('/panier-suppression' === '/' . $split[1]){
     CartController::delete($split[2]);
 }
 
-if('/reservation-details' === '/' . $split[1]){
-    Security::isloggedIn($_SESSION);
-    ReservationsController::getDetails($split[2]);
-}
-
 if('/calendrier' === $uri){
     Security::isloggedIn($_SESSION);
-    Main::calendar();
-}
-
-if('/reservation-impossible' == $uri){
-    ReservationsController::impossible();
+    CalendarController::calendar();
 }
 
 if('/vol-complet' == $uri){
     Security::isloggedIn($_SESSION);
     ReservationsController::full();
-}
-
-if('/reservation-acceptee' == $uri){
-    Security::isloggedIn($_SESSION);
-    ReservationsController::success();
 }
 
 if('/ajouter-vol' == $uri){
