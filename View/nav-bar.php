b2cfd28dab2b14545bc98e50ebbf12a2076405dd
VNSG <veens34@free.fr>
2022-11-23 10:16:24 +0100

    commit pour rien dans le but de pouvoir pull

--- View/nav-bar.php
+++ View/nav-bar.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 require('../Connection/Session.class.php');
 Session::creation();
@@ -13,32 +13,36 @@ Session::creation();
 
   <div class="collapse navbar-collapse" id="navbarsExampleDefault">
     <ul class="navbar-nav mr-auto">
-      <?php if(!empty($_SESSION['admin'])): ?>
+      <?php if (!empty($_SESSION['admin'])) : ?>
         <li class="nav-item text-black">
           <a class="nav-link text-black" href="./add-flight.php">Ajoutez un vol</a>
         </li>
       <?php endif ?>
-      <?php if(!empty($_SESSION['login'])): ?>
+      <?php if (!empty($_SESSION['login'])) : ?>
         <li class="nav-item active">
           <a class="nav-link" href="./reservation-personal-space.php">Réservations</a>
         </li>
         <li class="nav-item text-white">
           <a class="nav-link" href="./logout.php">Déconnexion</a>
         </li>
-        <li class="nav-item text-white cart">
-          <a class="nav-link cart" href="./cart.php"><i class="fa-solid fa-cart-shopping"></i></a>
-        </li>
+        <div class="cart-group">
+          <li class="nav-item text-white cart">
+            <a class="nav-link cart" href="./cart.php"><i class="fa-solid fa-cart-shopping"></i></a>
+          </li>
+          <p class="circle "></p>
+          </li><!-- <li class="nav-item text-white">
+            <p class="circle cart"></p>
+          </li>!-->
+        </div>
         <li class="nav-item text-white welcome-nav-bar">
           <p class="pt-4">Bienvenue <?php echo ucwords($_SESSION['login']); ?></p>
         </li>
-      <?php else: ?>
+      <?php else : ?>
         <li class="nav-item text-white">
           <a class="nav-link" href="./login.php">Connexion</a>
         </li>
       <?php endif; ?>
     </ul>
   </div>
-  
+
 </nav>
-
-
