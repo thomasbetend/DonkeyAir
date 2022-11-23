de55c3dd50e651b962b07b47e63a7cc7c3b8ad11
VNSG <veens34@free.fr>
2022-11-23 16:54:41 +0100

    modifs footer html css

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 <footer class="py-3 my-4 footer mt-auto py-3">
     <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-    </ul>
-    <p class="text-center text-footer-DonkeyAir">DonkeyAir</p>
+        <a class="text-center text-footer-DonkeyAir" href="https://donkey.school/" target="_blank">DonkeyAir By DonkeySchool</a>
+        <a class="btn btn-primary github" style="" href="https://github.com/thomasbetend/DonkeyAir" target="_blank" role="button"><i class="fab fa-github"></i></a>
+    </ul>  
 </footer>
