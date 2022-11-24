9435b683f1f66c1ec36052d20eeb6e552be38ecc
VNSG <veens34@free.fr>
2022-11-24 12:09:22 +0100

    modifs on footer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer class="py-3 my-4 footer mt-auto py-3">
-    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-        <a class="text-center text-footer-DonkeyAir" href="https://donkey.school/" target="_blank">DonkeyAir By DonkeySchool</a>
-        <a class="btn btn-primary github" style="" href="https://github.com/thomasbetend/DonkeyAir" target="_blank" role="button"><i class="fab fa-github"></i></a>
-    </ul>  
-</footer>
+   
+        <a class="text-center text-footer-DonkeyAir foot" href="https://donkey.school/" target="_blank">DonkeyAir </a>
+        <p class="footer-end">Développé par Donkeyschool 2022</p>
+  
+    </footer>
