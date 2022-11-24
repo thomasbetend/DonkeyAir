3b6a2889fa7d46077a96cc968ee57995a015a4dc
VNSG <veens34@free.fr>
2022-11-24 13:18:46 +0100

    little changements footer font and delete 2022

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer class="py-3 my-4 footer mt-auto py-3">
    
         <a class="text-center text-footer-DonkeyAir foot" href="https://donkey.school/" target="_blank">DonkeyAir </a>
-        <p class="footer-end">Développé par Donkeyschool 2022</p>
+        <p class="footer-end">Développé par Donkeyschool </p>
   
     </footer>
